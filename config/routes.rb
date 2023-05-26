c139d3c065724ff823553d2f8c0b838ddf37ec6c
Rob Young <rhyoung214@gmail.com>
2023-05-25 21:27:11 -0700

    add users controller and secure app with authenticate in app controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'public#index'
+  
   devise_for :users
+  
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
